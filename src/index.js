c184560ab8b0b3fb6e3e55f75c565d7bf46a0381
wxyzhenmiaotiao <wxyzhenmiaotiao>
2020-01-15 15:41:33 +0800

    aasd

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/lib/integration/react'
-import { store } from "./store"
-import Router from "./router"
-import "./style.less"
-import "./reset.css"
+import { store } from './store'
+import Router from './router'
+import './style.less'
+import './reset.css'
+import './rem.js'
 
 ReactDOM.render(
 	<Provider store={store}>
